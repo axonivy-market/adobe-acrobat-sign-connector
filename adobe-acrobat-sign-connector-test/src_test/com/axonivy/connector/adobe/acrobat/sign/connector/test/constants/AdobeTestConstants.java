7f7d1340f70ebea4a1b0f98a9f3120138d453f2f
Trần Vĩnh Thiện Phúc <phuc.tranvinhthien@axonactive.com>
2025-07-15 15:30:22 +0700

    MARP-1983 Update end2end UTs

---
+++ adobe-acrobat-sign-connector-test/src_test/com/axonivy/connector/adobe/acrobat/sign/connector/test/constants/AdobeTestConstants.java
@@ -0,0 +1,9 @@
+package com.axonivy.connector.adobe.acrobat.sign.connector.test.constants;
+
+public class AdobeTestConstants {
+  public static final String MOCK_SERVER_CONTEXT_DISPLAY_NAME = "Mock Server Test";
+  public static final String REAL_CALL_CONTEXT_DISPLAY_NAME = "Real Server Test";
+  public static final String INTERGRATION_KEY = "integrationKey";
+  public static final String RETURN_PAGE = "returnPage";
+  public static final String HOST = "host";
+}
