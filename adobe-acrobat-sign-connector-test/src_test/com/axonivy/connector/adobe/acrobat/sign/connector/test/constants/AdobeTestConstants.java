83d1cb04a970dbed60b3837699d854bf09562e26
Dinh Nguyen <dinh.nguyentrinhquoc@axonactive.com>
2025-07-30 21:21:15 +0700

    update workflow

--- adobe-acrobat-sign-connector-test/src_test/com/axonivy/connector/adobe/acrobat/sign/connector/test/constants/AdobeTestConstants.java
+++ adobe-acrobat-sign-connector-test/src_test/com/axonivy/connector/adobe/acrobat/sign/connector/test/constants/AdobeTestConstants.java
@@ -7,5 +7,5 @@ public class AdobeTestConstants {
   public static final String RETURN_PAGE = "returnPage";
   public static final String HOST = "host";
   public static final String END_TO_END_TESTING_ENVIRONMENT_KEY = "testEnvironment";
-  public static final String END_TO_END_TESTING_ENVIRONMENT_VALUE = "E2E Testing";
+  public static final String END_TO_END_TESTING_ENVIRONMENT_VALUE = "E2E";
 }
