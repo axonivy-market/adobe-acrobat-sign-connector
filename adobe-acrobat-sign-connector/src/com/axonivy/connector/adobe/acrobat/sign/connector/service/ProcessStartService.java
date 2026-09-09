88b705d79dc3220d0cef79d54f6756d3574531c6
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2026-09-09 13:57:18 +0200

    fix: migrate start-link retriever

--- adobe-acrobat-sign-connector/src/com/axonivy/connector/adobe/acrobat/sign/connector/service/ProcessStartService.java
+++ adobe-acrobat-sign-connector/src/com/axonivy/connector/adobe/acrobat/sign/connector/service/ProcessStartService.java
@@ -1,18 +1,8 @@
 package com.axonivy.connector.adobe.acrobat.sign.connector.service;
 
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Stream;
-
 import org.apache.commons.lang3.StringUtils;
 
-import ch.ivyteam.ivy.application.ActivityState;
-import ch.ivyteam.ivy.application.IApplication;
-import ch.ivyteam.ivy.application.IProcessModel;
-import ch.ivyteam.ivy.application.IProcessModelVersion;
-import ch.ivyteam.ivy.application.app.IApplicationRepository;
-import ch.ivyteam.ivy.environment.Ivy;
-import ch.ivyteam.ivy.security.ISecurityContext;
+import ch.ivyteam.ivy.application.project.Project;
 import ch.ivyteam.ivy.security.exec.Sudo;
 import ch.ivyteam.ivy.workflow.IWorkflowProcessModelVersion;
 import ch.ivyteam.ivy.workflow.start.IWebStartable;
@@ -36,40 +26,11 @@ public class ProcessStartService {
 
 	private static IWebStartable findProcessStartByUserFriendlyRequestPath(String requestPath) {
 		return Sudo.get(() -> {
-			IWebStartable processStart = getProcessStart(requestPath, Ivy.request().getProcessModelVersion());
-			if (processStart != null) {
-				return processStart;
-			}
-			List<IApplication> applicationsInSecurityContext = IApplicationRepository.of(ISecurityContext.current()).all();
-			for (IApplication app : applicationsInSecurityContext) {
-				IWebStartable findProcessStart = filterPMV(requestPath, app).findFirst().orElse(null);
-				if (findProcessStart != null) {
-					return findProcessStart;
-				}
-			}
-			return null;
+			var pmv = IWorkflowProcessModelVersion.of(Project.current());
+			return pmv.getAllStartables()
+					.filter(start -> start.getId().contains(requestPath))
+					.findAny()
+					.orElse(null);
 		});
-	}
-
-	private static IWebStartable getProcessStart(String requestPath, IProcessModelVersion processModelVersion) {
-		return IWorkflowProcessModelVersion.of(processModelVersion).getAllStartables()
-				.filter(start -> StringUtils.contains(start.getId(), requestPath)).findAny().orElse(null);
-	}
-
-	private static Stream<IWebStartable> filterPMV(String requestPath, IApplication application) {
-		return filterActivePMVOfApp(application).map(p -> getProcessStart(requestPath, p)).filter(Objects::nonNull);
-	}
-
-	private static Stream<IProcessModelVersion> filterActivePMVOfApp(IApplication application) {
-		return application.getProcessModelsSortedByName().stream().filter(pm -> isActive(pm))
-				.map(IProcessModel::getReleasedProcessModelVersion).filter(pmv -> isActive(pmv));
-	}
-
-	private static boolean isActive(IProcessModelVersion processModelVersion) {
-		return processModelVersion != null && processModelVersion.getActivityState() == ActivityState.ACTIVE;
-	}
-
-	private static boolean isActive(IProcessModel processModel) {
-		return processModel.getActivityState() == ActivityState.ACTIVE;
 	}
 }
